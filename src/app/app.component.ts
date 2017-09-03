249e164e243e1beaf517be7c12f7fe24fe98fb62
Herri Fransisca <herrifransisca@outlook.com>
2017-09-03 16:38:28 +0700

    180. Updating an Object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   course$;
   author$;
 
-  constructor(db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase) {
     this.courses$ = db.list('/courses');
     this.course$ = db.object('/courses/1');
     this.author$ = db.object('/authors/1');
@@ -35,5 +35,33 @@ export class AppComponent {
     });
 
     course.value = '';
+  }
+
+  update(course) {
+    // this.db.object('/courses/' + course.$key)
+    //   .set(course.$value + 'UPDATED');
+
+    // we can also pass a "complex object"
+    // this.db.object('/courses/' + course.$key)
+    //   .set({
+    //     title: course.$value + ' UPDATED',
+    //     price: 150
+    //   });
+
+    // using "update"
+    this.db.object('/courses/' + course.$key)
+      .update({
+        title: 'New Title',
+        isLive: true
+      });
+
+    
+    // SUMMARY
+    // we can update using : .set & .update
+    // what's the difference ?
+    // set => the value of object will replace ENTIRELY with the object we passed her
+    // update => only updates the properties we listed here, 
+    //           if this property exist, they would be updated 
+    //                                   otherwise they would be added.
   }
 }
