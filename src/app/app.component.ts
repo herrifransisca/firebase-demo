cba32e01d11f67af238ab873dbd05e350bc02f64
Herri Fransisca <herrifransisca@outlook.com>
2017-09-03 12:31:11 +0700

    172. Reading Lists.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses: any[];
+
+  constructor(db: AngularFireDatabase) {
+    db.list('/courses')
+      .subscribe(courses => {
+        this.courses = courses;
+        console.log(courses);
+      });
+  }
 }
